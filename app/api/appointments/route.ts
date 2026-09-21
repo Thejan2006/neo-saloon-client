cd7c6f2f2bacad3b4f2e036035da54233b3276c1
Thejan2006 <hmagthejanbandar@gmail.com>
2026-09-21 19:47:19 +0530

    Add POST route for appointments with user retrieval

---
+++ app/api/appointments/route.ts
@@ -0,0 +1,11 @@
+import { getUser } from "@/utils/authentication";
+import { NextRequest } from "next/server";
+
+export function POST(request : NextRequest){
+    
+    const user = getUser(request)
+
+    console.log("User: ", user)
+
+
+}
