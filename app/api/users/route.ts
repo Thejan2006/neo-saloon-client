8764c81cdb4dae6f79f423dfe193b9e11b562ed8
Thejan2006 <hmagthejanbandar@gmail.com>
2026-09-26 21:54:56 +0530

    Implement Zod validation for user registration and self-update requests

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,9 @@ import prisma from "@/lib/prisma";
 import { getUser, isPrivileged } from "@/utils/authentication";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-
+import { z } from "zod";
+import { UserRegistrationRequestSchema } from "@/types/dto/UserRegistrationRequest";
+import { UserSelfUpdateRequestSchema } from "@/types/dto/UserSelfUpdateRequest";
 export async function GET(request : NextRequest){
 
     const havePrivilege = await isPrivileged(request, "users:read")
@@ -82,91 +84,80 @@ export async function POST(request : NextRequest){
 
     const body = await request.json()
 
-    if(body.email == null){
-        return NextResponse.json(
+    //validate the body using zod
+
+    try{
+
+        const parsedBody = UserRegistrationRequestSchema.parse(body)
+
+        const existingUser = await prisma.user.findUnique(
             {
-                message : "Email is required"
-            },
-            {
-                status : 422
+                where : {
+                    email : parsedBody.email
+                }
             }
         )
-    }
 
-    if(body.firstName == null){
-        return NextResponse.json(
-            {
-                message : "First name is required"
-            },
-            {
-                status : 422
-            }
-        )
-    }
-
-    if(body.lastName == null){
-        return NextResponse.json(
-            {
-                message : "Last name is required"
-            },
-            {
-                status : 422
-            }
-        )
-    }
-
-    if(body.password == null){
-        return NextResponse.json(
-            {
-                message : "Password is required"
-            },
-            {
-                status : 422
-            }
-        )
-    }
-
-    const existingUser = await prisma.user.findUnique(
-        {
-            where : {
-                email : body.email
-            }
+        if(existingUser != null){
+            return NextResponse.json(
+                {
+                    message : "User with this email already exists"
+                },
+                {
+                    status : 409
+                }
+            )
         }
-    )
 
-    if(existingUser != null){
+        const passwordHash = await bcrypt.hash(body.password, 12)
+
+        await prisma.user.create({
+            data :{
+                email : body.email,
+                firstname : body.firstName,
+                lastname : body.lastName,
+                password : passwordHash,
+                phone : body.phone,
+            }
+        })
+
         return NextResponse.json(
             {
-                message : "User with this email already exists"
+                message : "User created successfully"
             },
             {
-                status : 409
+                status : 201
             }
         )
+    }catch(error){
+        
+        if(error instanceof z.ZodError){
+
+            // console.log(error.issues[0]?.message ?? "Invalid input")
+
+            return NextResponse.json(
+                {
+                    message : error.issues[0]?.message ?? "Invalid input", // if the error message is not available, return "Invalid input"
+                },
+                {
+                    status : 400
+                }
+            )
+
+        }
+
+        console.log(error)
+        return NextResponse.json(
+            {
+                message : "Invalid request body",
+                error : error
+            },
+            {
+                status : 400
+            }
+        )
+
     }
-
-    const passwordHash = await bcrypt.hash(body.password, 12)
-
-    await prisma.user.create({
-        data :{
-            email : body.email,
-            firstname : body.firstName,
-            lastname : body.lastName,
-            password : passwordHash,
-            phone : body.phone,
-        }
-    })
-
-    //SELECT 
-
-    return NextResponse.json(
-        {
-            message : "User created successfully"
-        },
-        {
-            status : 201
-        }
-    )
 
 }
 
@@ -187,106 +178,134 @@ export async function PUT(request : NextRequest){
         )
     }
 
-    const body = await request.json()
+    try{
 
-    if(requestedUser.id == id){
-        
-        //never allow users to update their own role, status, privileges
+        const body = await request.json()
 
-        const user = await prisma.user.findUnique({
-            where : {
-                id : id
+        if(requestedUser.id == id){
+            
+            //never allow users to update their own role, status, privileges
+
+            UserSelfUpdateRequestSchema.parse(body)
+            const user = await prisma.user.findUnique({
+                where : {
+                    id : id
+                }
+            })
+
+            if(user == null){
+                return NextResponse.json(
+                    {
+                        message : "User not found"
+                    },
+                    {
+                        status : 404
+                    }
+                )
             }
-        })
 
-        if(user == null){
+            await prisma.user.update({
+
+                where : {
+                    id : id
+                },
+                data : {
+                    email : body.email || user.email,
+                    firstName : body.firstName || user.firstname,
+                    lastName : body.lastName || user.lastname,
+                    phone : body.phone || user.phone,
+                    profileImage : body.profileImage || user.profileImage // should be included in the token
+                }
+
+            })
+
             return NextResponse.json(
                 {
-                    message : "User not found"
+                    message : "User updated successfully"
+                }
+            )
+
+        }else{
+            
+            const havePrivilege = await isPrivileged(request, "users:edit")           
+
+            if(!havePrivilege){
+                return NextResponse.json(
+                    {
+                        message : "You do not have the privilege to edit other users"
+                    },
+                    {
+                        status : 403
+                    }
+                )
+            }
+
+            UserUpdateByAdminRequestSchema.parse(body)
+
+            const user = await prisma.user.findUnique({
+                where : {
+                    id : id||"000"
+                }
+            })
+
+
+
+            if(user == null){
+                return NextResponse.json(
+                    {
+                        message : "User not found"
+                    },
+                    {
+                        status : 404
+                    }
+                )
+            }
+            
+            await prisma.user.update({
+                where : {
+                    id : id||"000"
+                },
+                data : {
+                    email : body.email || user.email,
+                    firstname : body.firstName || user.firstname,
+                    lastname : body.lastName || user.lastname,
+                    phone : body.phone || user.phone,
+                    profileImage : body.profileImage || user.profileImage,
+                    role : body.role || user.role,
+                    status : body.status || user.status,
+                    privileges : body.privileges || user.privileges
+                }
+            })
+
+            return NextResponse.json(
+                {
+                    message : "User updated successfully"
+                }
+            )
+            
+        }
+
+    }catch(error){
+
+        if(error instanceof z.ZodError){
+            return NextResponse.json(
+                {
+                    message : error.issues[0]?.message ?? "Invalid input",
                 },
                 {
-                    status : 404
+                    status : 400
                 }
             )
         }
-
-        await prisma.user.update({
-
-            where : {
-                id : id
-            },
-            data : {
-                email : body.email || user.email,
-                firstname : body.firstName || user.firstname,
-                lastname : body.lastName || user.lastname,
-                phone : body.phone ?? user.phone,
-                profileImage : body.profileImage ?? user.profileImage
-            }
-
-        })
 
         return NextResponse.json(
             {
-                message : "User updated successfully"
-            }
-        )
-
-    }else{
-       
-        const havePrivilege = await isPrivileged(request, "users:edit")
-
-
-        if(!havePrivilege){
-            return NextResponse.json(
-                {
-                    message : "You do not have the privilege to edit other users"
-                },
-                {
-                    status : 403
-                }
-            )
-        }
-
-        const user = await prisma.user.findUnique({
-            where : {
-                id : id||"000"
-            }
-        })
-
-        if(user == null){
-            return NextResponse.json(
-                {
-                    message : "User not found"
-                },
-                {
-                    status : 404
-                }
-            )
-        }
-        
-        await prisma.user.update({
-            where : {
-                id : id||"000"
+                message : "Server error"
             },
-            data : {
-                email : body.email || user.email,
-                firstname : body.firstName || user.firstname,
-                lastname : body.lastName || user.lastname,
-                phone : body.phone ?? user.phone,
-                profileImage : body.profileImage ?? user.profileImage,
-                role : body.role || user.role,
-                status : body.status || user.status,
-                privileges : body.privileges || user.privileges
-            }
-        })
-
-        return NextResponse.json(
             {
-                message : "User updated successfully"
+                status : 500
             }
         )
-        
-    }
-    
+    }  
 }
 
